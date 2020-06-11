bf52801f4e4088a0b344caab02ff71abf14c1449
Curt Grogan <curtgrogan@outlook.com>
2020-06-10 20:48:45 -0400

    *ngIf

---
+++ src/app/ng-if/ng-if.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ng-if",
+  templateUrl: "./ng-if.component.html",
+  styleUrls: ["./ng-if.component.scss"],
+})
+export class NgIfComponent {
+  courses = [];
+  constructor() {
+    this.courses = ["Angular 101", "Angular 123", "Angular 307"];
+  }
+}
